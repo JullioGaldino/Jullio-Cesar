ca7bb6df0a386e11224e168829df619926f97b0c
Júllio César <julliocesar5002@gmail.com>
2024-09-07 12:12:03 -0300

    modified:   data/profile1.json

--- assets/js/api.js
+++ assets/js/api.js
@@ -1,5 +1,5 @@
 async function fetchProfileData() {
-   const url = 'https://raw.githubusercontent.com/JullioGaldino/Jullio-Cesar/main/profile.json?token=GHSAT0AAAAAACXAQC7OWFTO77ZCXC57MDI2ZWYLSZQ';
+   const url = 'https://raw.githubusercontent.com/JullioGaldino/Jullio-Cesar/main/data/profile1.json';
    const fetching = await fetch(url);
    return await fetching.json();
 }
