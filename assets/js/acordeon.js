d0069bcb27277a6a5b5b8a3a7910d9aed9d648b2
Júllio César <julliocesar5002@gmail.com>
2024-09-10 08:41:54 -0300

    Js modified

--- assets/js/acordeon.js
+++ assets/js/acordeon.js
@@ -13,3 +13,16 @@ acordeonTriggers.forEach((trigger) => {
       }
    });
 });
+headerafters.forEach((after) => {
+
+   trigger.addEventListener('click', (e) => {
+      const header = after.parentElement;
+      const isOpen = header.classList.contains('open');
+
+      if(isOpen) {
+         acordeon.classList.remove('open');
+      } else {
+         acordeon.classList.add('open');
+      }
+   });
+});
